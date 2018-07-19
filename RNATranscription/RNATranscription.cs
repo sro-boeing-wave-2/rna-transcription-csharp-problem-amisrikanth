8f7413ba90b83a92450df44efd442cdecd26c413
Srikanth Banagadi <amisrikanth@gmail.com>
2018-07-19 10:41:16 +0530

    completed the assignment

--- RNATranscription/RNATranscription.cs
+++ RNATranscription/RNATranscription.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace RNATranscriptionModule
 {
@@ -6,7 +8,18 @@ namespace RNATranscriptionModule
     {
         public static string ToRna(string nucleotide)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            Dictionary<char, char> conversionToRNA = new Dictionary<char, char>();
+            conversionToRNA.Add('G', 'C');
+            conversionToRNA.Add('C', 'G');
+            conversionToRNA.Add('T', 'A');
+            conversionToRNA.Add('A', 'U');
+            StringBuilder convertedRNA = new StringBuilder();
+            for(int i=0;i<nucleotide.Length;i++)
+            {
+                convertedRNA.Append(conversionToRNA[nucleotide[i]]);
+            }
+            return convertedRNA.ToString();
+
         }
     }
 }
